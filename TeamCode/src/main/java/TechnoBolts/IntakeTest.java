60b60e0a45e386c59f838b69bcb65147b05d023e
TechnoBolts <ftctechnobolts@gmail.com>
2025-11-05 20:58:15 -0500

    11/5/25 PS
    Fixed config for motors, added Intake motor code

---
+++ TeamCode/src/main/java/TechnoBolts/IntakeTest.java
@@ -0,0 +1,39 @@
+package TechnoBolts;
+
+
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp(name="DecodeIntakeTest", group="TechnoBolts - OpMode")
+public class IntakeTest extends LinearOpMode {
+    //private ElapsedTime runtime = new ElapsedTime();
+    public DcMotor Intake = null;
+
+
+    @Override
+    public void runOpMode() {
+
+
+        // Initialize the hardware variables. Note that the strings used here must correspond
+        // to the names assigned during the robot configuration step on the DS or RC devices.
+        Intake = hardwareMap.get(DcMotor.class, "Intake");
+
+        waitForStart();
+        // runtime.reset();
+
+        while (opModeIsActive()) {
+
+
+            if (gamepad1.a)
+                Intake.setPower(-1);
+            if (gamepad1.b)
+                Intake.setPower(0);
+        }
+    }
+}
+
