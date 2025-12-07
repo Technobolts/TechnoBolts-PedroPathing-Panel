3dbb424436abc405d4e0303063f844cd0ad5e990
aj25687 <javantika.jain@gmail.com>
2025-12-07 14:21:15 -0500

    12/7/25 Added LED light control

--- TeamCode/src/main/java/TechnoBolts/OutakeTest.java
+++ TeamCode/src/main/java/TechnoBolts/OutakeTest.java
@@ -3,6 +3,7 @@ package TechnoBolts;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.LED;
 
 @TeleOp(name="OutakeTest", group="Linear Opmode")
     public class OutakeTest extends LinearOpMode {
@@ -14,7 +15,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 
             DcMotor myMotorLeft = hardwareMap.get(DcMotor.class, "leftDeposit");
             DcMotor myMotorRight = hardwareMap.get(DcMotor.class, "rightDeposit");
-
+            LED ledDepo = hardwareMap.get(LED.class,"ledDepo" );
             // Set the motor direction if needed (e.g., if it spins backward)
              myMotorLeft.setDirection(DcMotor.Direction.FORWARD);
             myMotorRight.setDirection(DcMotor.Direction.FORWARD);
@@ -29,6 +30,9 @@ import com.qualcomm.robotcore.hardware.DcMotor;
                 // Example: Control motor with gamepad input
                 while(gamepad1.left_bumper) {
                     motorPower += 10; // Use left bumper for power
+                    if(motorPower <= 1000) ledDepo.enableLight(true);
+                    else ledDepo.enableLight(false);
+
                 }
                 myMotorLeft.setPower(motorPower); // Set the motor power
                 myMotorRight.setPower(motorPower);
