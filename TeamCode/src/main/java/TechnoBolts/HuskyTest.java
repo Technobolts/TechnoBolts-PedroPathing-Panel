6304e9fb07d2f9af80a788f19a05f44484612655
aj25687 <javantika.jain@gmail.com>
2025-12-21 16:53:16 -0500

    12/21/25 AJ

--- TeamCode/src/main/java/TechnoBolts/HuskyTest.java
+++ TeamCode/src/main/java/TechnoBolts/HuskyTest.java
@@ -1,4 +1,18 @@
 package TechnoBolts;
 
-public class HuskyTest {
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp(name="HuskyTest")
+public class HuskyTest extends LinearOpMode{
+    @Override
+    public void runOpMode() throws InterruptedException {
+        Servo huskyServo = hardwareMap.get(Servo.class, "huskyServo");
+
+
+    }
 }
+
+
+
