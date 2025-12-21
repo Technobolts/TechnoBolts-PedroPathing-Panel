60e34fea606c21e93a467999c59878d34f2fd83e
aj25687 <javantika.jain@gmail.com>
2025-12-21 17:01:18 -0500

    12/21/25 AJ

--- TeamCode/src/main/java/TechnoBolts/HuskyTest.java
+++ TeamCode/src/main/java/TechnoBolts/HuskyTest.java
@@ -3,14 +3,26 @@ package TechnoBolts;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
+import
 
 @TeleOp(name="HuskyTest")
 public class HuskyTest extends LinearOpMode{
     @Override
     public void runOpMode() throws InterruptedException {
         Servo huskyServo = hardwareMap.get(Servo.class, "huskyServo");
+        TechnoBoltsHuskyLensDistance distance = new TechnoBoltsHuskyLensDistance();
+        while (opModeIsActive()) {
 
-
+            while(gamepad2.left_bumper) {
+                //while(!apriltagNotDetected){
+                //  servo spins around
+                // }
+                //use distance(var) and get a distance from april tag
+                //put that distance in calculateShooterAngle
+                //set rpm of the flywheels to calculated rpm
+                //green light once everything finishes
+            }
+        }
     }
 }
 
