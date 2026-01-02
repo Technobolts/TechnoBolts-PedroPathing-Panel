be07ff4dbf1be005240ae24cc1b53a405bf946b2
aj25687 <javantika.jain@gmail.com>
2026-01-02 15:11:57 -0500

    1/2/26 AJ

--- TeamCode/src/main/java/TechnoBolts/RPMAndLEDTesting.java
+++ TeamCode/src/main/java/TechnoBolts/RPMAndLEDTesting.java
@@ -48,10 +48,16 @@ public class RPMAndLEDTesting extends LinearOpMode {
                 leftDeposit.setPower(0); // Set the motor power
                 rightDeposit.setPower(0);
             }
-            double leftVelocity = leftDeposit.getVelocity();
-            double rightVelocity = rightDeposit.getVelocity();
+            double leftVelocity = leftDeposit.getPower();
+            double rightVelocity = rightDeposit.getPower();
             telemetry.addData("Velocity left/Right", "%4.2f, %4.2f", leftVelocity, rightVelocity);
             telemetry.update();
+
+//            if(leftVelocity<0.4 && rightVelocity<0.4){
+//
+//
+//
+//            }
 
 //            long leftCurrentPosition = leftDeposit.getCurrentPosition();
 //            // Calculate difference since last loop
