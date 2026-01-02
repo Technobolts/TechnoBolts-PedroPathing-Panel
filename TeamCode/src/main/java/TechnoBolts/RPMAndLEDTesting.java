7714a5cdde5fda7ae46faedfbe2538797ce3115a
aj25687 <javantika.jain@gmail.com>
2026-01-02 14:54:24 -0500

    1/2/26 AJ

--- TeamCode/src/main/java/TechnoBolts/RPMAndLEDTesting.java
+++ TeamCode/src/main/java/TechnoBolts/RPMAndLEDTesting.java
@@ -3,7 +3,6 @@ package TechnoBolts;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.Servo; // Changed from CRServo
-import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
