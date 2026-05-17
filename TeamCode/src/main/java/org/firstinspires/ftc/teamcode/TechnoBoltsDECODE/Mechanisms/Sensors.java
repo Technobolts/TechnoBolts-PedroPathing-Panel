9fa8dda7131df151b9db671f888265400290b2f0
aj25687 <javantika.jain@gmail.com>
2026-05-17 15:24:21 -0400

    5/17/26 AJ

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TechnoBoltsDECODE/Mechanisms/Sensors.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TechnoBoltsDECODE/Mechanisms/Sensors.java
@@ -3,27 +3,51 @@ package org.firstinspires.ftc.teamcode.TechnoBoltsDECODE.Mechanisms;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
 import com.qualcomm.robotcore.hardware.NormalizedRGBA;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 public class Sensors {
-
     NormalizedColorSensor colorSensor;
 
-    public enum detectedColor{
+    public enum DetectedColor {
         GREEN,
         PURPLE,
         UNKNOWN
     }
-    public  void init(HardwareMap hwMap){
+
+    public void init(HardwareMap hwMap){
         colorSensor = hwMap.get(NormalizedColorSensor.class, "color_sensor_1");
     }
-    public DetectedColor getDetectedColor(){
+
+    public DetectedColor getDetectedColor(Telemetry telemetry){
         NormalizedRGBA colors = colorSensor.getNormalizedColors();
 
-        float normPurple, normGreen;
-        normGreen = colors.green / colors.alpha;
+        float[] hsv = new float[3];
 
+        // 2. Convert the normalized colors (scaled 0-255) to HSV
+        android.graphics.Color.RGBToHSV(
+                (int)(colors.red * 255),
+                (int)(colors.green * 255),
+                (int)(colors.blue * 255),
+                hsv
+        );
+
+        // 3. Extract the Hue and Saturation using proper array indices
+        float hue = hsv[0];
+        float saturation = hsv[1];
+
+        // Telemetry for easy calibration debugging
+        telemetry.addData("Current Hue", hue);
+        telemetry.addData("Current Saturation", saturation);
+
+        // 4. Threshold Logic (Adjust these numbers based on your field tests)
+        if (hue >= 100 && hue <= 140 && saturation > 0.4) {
+            return DetectedColor.GREEN;
+        } else if (hue >= 165 && hue <= 240 && saturation > 0.4) {
+            return DetectedColor.PURPLE;
+        }
+
+        return DetectedColor.UNKNOWN;
     }
-
 
 
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TechnoBoltsDECODE/TeleOp/ColorSensorTest.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode.TechnoBoltsDECODE.TeleOp;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+public class ColorSensorTest extends OpMode {
+
+    @Override
+    public void init(){
+
+    }
+    @Override
+    public void loop(){
+
+    }
+}
+
