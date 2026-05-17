4646def79ebcdea0e62db757a26de6baaaf0447a
r2sahouse-create <r2sahouse@gmail.com>
2026-05-17 16:39:19 -0400

    5/17/26

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TechnoBoltsDECODE/MagnetTest.java
@@ -0,0 +1,37 @@
+package org.firstinspires.ftc.teamcode.TechnoBoltsDECODE;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+
+@TeleOp(name = "Magnet Switch Test")
+public class MagnetTest extends OpMode {
+
+    private DigitalChannel magnetSwitch;
+
+    @Override
+    public void init() {
+        // Hardware name must match your configuration EXACTLY
+        magnetSwitch = hardwareMap.get(DigitalChannel.class, "magnet_switch_sensor");
+
+        // REV Magnetic Switch is a digital input
+        magnetSwitch.setMode(DigitalChannel.Mode.INPUT);
+
+        telemetry.addLine("Magnet Switch Initialized");
+    }
+
+    @Override
+    public void loop() {
+        boolean isPressed = !magnetSwitch.getState();
+        // REV magnetic switch returns:
+        // getState() == true  → NOT pressed (open)
+        // getState() == false → PRESSED (closed)
+
+        telemetry.addData("Magnet State", isPressed ? "CLOSED" : "OPEN");
+        telemetry.update();
+    }
+}
+
+
+
+
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TechnoBoltsDECODE/Mechanisms/magnetic_limit_sensor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TechnoBoltsDECODE/Mechanisms/magnetic_limit_sensor.java
@@ -2,24 +2,25 @@ package org.firstinspires.ftc.teamcode.TechnoBoltsDECODE.Mechanisms;
 
 import androidx.annotation.NonNull;
 
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
-import org.firstinspires.ftc.ftccommon.internal.manualcontrol.parameters.DigitalChannelValueParameters;
-
-@TeleOp
 public class magnetic_limit_sensor {
 
-    private DigitalChannelValueParameters magnetic_limit_sensor;
+    private DigitalChannel magneticLimitSensor;
 
-  public void init(@NonNull HardwareMap hwMap) {
-      magnetic_limit_sensor = hwMap.get(DigitalChannel.class, "magnet_limit_sensor");
-      magnetic_limit_sensor. setMode(DigitalChannel.Mode.INPUT);
+    public void init(@NonNull HardwareMap hwMap) {
+        magneticLimitSensor = hwMap.get(DigitalChannel.class, "magnet_limit_sensor");
+        magneticLimitSensor.setMode(DigitalChannel.Mode.INPUT);
+    }
+
+    public boolean getMagneticLimitSensor() {
+        // REV magnetic switch is ACTIVE-LOW:
+        // getState() == true  → OPEN (not triggered)
+        // getState() == false → CLOSED (triggered)
+        return !magneticLimitSensor.getState();
+    }
 }
 
-public boolean getmagneticlimitsensor(){
-      return !LimitMagnet.getState();
 
-}
 
