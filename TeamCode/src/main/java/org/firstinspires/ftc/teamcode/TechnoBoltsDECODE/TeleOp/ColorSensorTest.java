0025e896ca675967355ab1c35df27d772ba16c2f
aj25687 <javantika.jain@gmail.com>
2026-05-17 15:25:30 -0400

    5/17/26 AJ

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TechnoBoltsDECODE/TeleOp/ColorSensorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TechnoBoltsDECODE/TeleOp/ColorSensorTest.java
@@ -2,15 +2,19 @@ package org.firstinspires.ftc.teamcode.TechnoBoltsDECODE.TeleOp;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
+import org.firstinspires.ftc.teamcode.TechnoBoltsDECODE.Mechanisms.Sensors;
+
 public class ColorSensorTest extends OpMode {
+
+    Sensors sensor = new Sensors();
 
     @Override
     public void init(){
-
+        sensor.init(hardwareMap);
     }
     @Override
     public void loop(){
-
+        sensor.getDetectedColor(telemetry);
     }
 }
 
