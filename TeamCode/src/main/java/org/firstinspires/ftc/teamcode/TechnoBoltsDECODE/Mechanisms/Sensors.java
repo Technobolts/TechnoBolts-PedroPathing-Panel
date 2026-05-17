73c0bf65523e1651e1fb9d5a8a6462a147c7ec84
aj25687 <javantika.jain@gmail.com>
2026-05-17 15:14:49 -0400

    5/17/26 AJ

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TechnoBoltsDECODE/Mechanisms/Sensors.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TechnoBoltsDECODE/Mechanisms/Sensors.java
@@ -1,4 +1,29 @@
 package org.firstinspires.ftc.teamcode.TechnoBoltsDECODE.Mechanisms;
 
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
+import com.qualcomm.robotcore.hardware.NormalizedRGBA;
+
 public class Sensors {
+
+    NormalizedColorSensor colorSensor;
+
+    public enum detectedColor{
+        GREEN,
+        PURPLE,
+        UNKNOWN
+    }
+    public  void init(HardwareMap hwMap){
+        colorSensor = hwMap.get(NormalizedColorSensor.class, "color_sensor_1");
+    }
+    public DetectedColor getDetectedColor(){
+        NormalizedRGBA colors = colorSensor.getNormalizedColors();
+
+        float normPurple, normGreen;
+        normGreen = colors.green / colors.alpha;
+
+    }
+
+
+
 }
