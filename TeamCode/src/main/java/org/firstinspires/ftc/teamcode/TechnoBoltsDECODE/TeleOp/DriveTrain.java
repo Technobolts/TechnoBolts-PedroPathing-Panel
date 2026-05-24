25c7aee264caeef138125e20c37a48e500596575
aj25687 <javantika.jain@gmail.com>
2026-05-24 14:51:44 -0400

    5/24/26 AJ

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TechnoBoltsDECODE/TeleOp/DriveTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TechnoBoltsDECODE/TeleOp/DriveTrain.java
@@ -2,4 +2,17 @@ package org.firstinspires.ftc.teamcode.TechnoBoltsDECODE.TeleOp;
 
 public class DriveTrain {
 
+    /*
+    * EXPANSION HUB CONFIG
+    * Motors:
+    * front-right:
+    * front-left:
+    * back-right:
+    * back-left:
+    *
+    * Encoders:
+    * encoder-right:
+    * encoder-left:
+    * */
+
 }
