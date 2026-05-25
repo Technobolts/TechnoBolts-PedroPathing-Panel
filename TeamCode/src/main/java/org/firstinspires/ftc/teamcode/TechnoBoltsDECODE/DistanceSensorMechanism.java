7fd94578b014296425845f4d7a2143f711c26ee8
aj25687 <javantika.jain@gmail.com>
2026-05-25 15:53:26 -0400

    5/25/26 AJ

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TechnoBoltsDECODE/DistanceSensorMechanism.java
@@ -0,0 +1,24 @@
+package  org.firstinspires.ftc.teamcode.TechnoBoltsDECODE;
+
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+public class DistanceSensorMechanism {
+    // Declare the DistanceSensor object
+    private DistanceSensor distanceSensor;
+
+    // Constructor: Maps the sensor using the hardwareMap passed from the OpMode
+    public DistanceSensorMechanism(HardwareMap hardwareMap) {
+        // "distance_sensor" must exactly match the name in your Driver Station configuration
+        distanceSensor = hardwareMap.get(DistanceSensor.class, "distance_sensor");
+    }
+
+    /**
+     * Getter method to return the current distance reading in inches.
+     * You can swap DistanceUnit.INCH for DistanceUnit.CM if you prefer centimeters.
+     */
+    public double getDistanceInches() {
+        return distanceSensor.getDistance(DistanceUnit.INCH);
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TechnoBoltsDECODE/DistanceSensorOpMode.java
@@ -0,0 +1,35 @@
+package  org.firstinspires.ftc.teamcode.TechnoBoltsDECODE;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.TechnoBoltsDECODE.DistanceSensorMechanism;
+
+@TeleOp(name = "Distance Sensor Test", group = "Tutorials")
+public class DistanceSensorOpMode extends LinearOpMode {
+
+    // Declare a reference to our mechanism class
+    private DistanceSensorMechanism sensorSubsystem;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        // Initialize the mechanism by passing this OpMode's hardwareMap
+        sensorSubsystem = new DistanceSensorMechanism(hardwareMap);
+
+        // Wait for the driver to press the PLAY button
+        waitForStart();
+
+        // Loop continuously while the OpMode is running
+        while (opModeIsActive()) {
+            // Get the live distance from our mechanism
+            double currentDistance = sensorSubsystem.getDistanceInches();
+
+            // Display the data on the Driver Station screen
+            telemetry.addData("Distance (Inches)", "%.2f", currentDistance);
+            telemetry.update();
+
+            // Small optional sleep to keep the loop from running overly fast
+            sleep(20);
+        }
+    }
+}
