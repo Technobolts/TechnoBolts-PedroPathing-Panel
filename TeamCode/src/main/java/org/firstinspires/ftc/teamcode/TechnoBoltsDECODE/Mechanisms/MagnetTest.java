c6b7bfd06122600d58397a4a95622b2d4bf14b73
aj25687 <javantika.jain@gmail.com>
2026-06-27 14:59:28 -0400

    6/27/26 AJ

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TechnoBoltsDECODE/Mechanisms/MagnetTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TechnoBoltsDECODE/Mechanisms/MagnetTest.java
@@ -2,37 +2,22 @@ package org.firstinspires.ftc.teamcode.TechnoBoltsDECODE.Mechanisms;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DigitalChannel;
+import org.firstinspires.ftc.teamcode.TechnoBoltsDECODE.Mechanisms.MagneticLimitSensor;
 
-@TeleOp(name = "Magnet Switch Test")
+@TeleOp(name = "Mechanism Class Test")
 public class MagnetTest extends OpMode {
 
-
-    private DigitalChannel magnetSwitch;
+    // Instantiate your custom mechanism class
+    MagneticLimitSensor limitSensor = new MagneticLimitSensor();
 
     @Override
     public void init() {
-        // Hardware name must match your configuration EXACTLY
-        magnetSwitch = hardwareMap.get(DigitalChannel.class, "magnet_limit_sensor");
-
-        // REV Magnetic Switch is a digital input
-        magnetSwitch.setMode(DigitalChannel.Mode.INPUT);
-
-        telemetry.addLine("Magnet Limit Initialized");
+        limitSensor.init(hardwareMap);
     }
 
     @Override
     public void loop() {
-        boolean isPressed = !magnetSwitch.getState();
-        // REV magnetic switch returns:
-        // getState() == true  → NOT pressed (open)
-        // getState() == false → PRESSED (closed)
-
-        telemetry.addData("Magnet State", isPressed ? "CLOSED" : "OPEN");
+        telemetry.addData("Is Magnet Close?", limitSensor.isPressed());
         telemetry.update();
     }
 }
-
-
-
-
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TechnoBoltsDECODE/Mechanisms/MagneticLimitSensor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TechnoBoltsDECODE/Mechanisms/MagneticLimitSensor.java
@@ -1,26 +1,19 @@
-//package org.firstinspires.ftc.teamcode.TechnoBoltsDECODE.Mechanisms;
-//import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-//import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-//import com.qualcomm.robotcore.hardware.DigitalChannel;
-//import com.qualcomm.robotcore.hardware.HardwareMap;
-//
-//@TeleOp
-//public class MagneticLimitSensor {
-//
-//    private DigitalChannel magneticLimitSensor extends OpMode;
-//
-//    @Override
-//    public void init(HardwareMap hwMap) {
-//        magneticLimitSensor = hwMap.get(DigitalChannel.class, "magnet_limit_sensor");
-//        magneticLimitSensor.setMode(DigitalChannel.Mode.INPUT);
-//    }
-//
-//    @Override
-//    public void loop() {
-//        public boolean getMagneticLimitSensor () {
-//            return !magneticLimitSensor.getState();
-//        }
-//
-//    }
-//
-//}
+package org.firstinspires.ftc.teamcode.TechnoBoltsDECODE.Mechanisms;
+
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class MagneticLimitSensor {
+
+    private DigitalChannel magneticLimitSensor;
+
+    public void init(HardwareMap hwMap) {
+        magneticLimitSensor = hwMap.get(DigitalChannel.class, "magnet_limit_sensor");
+        magneticLimitSensor.setMode(DigitalChannel.Mode.INPUT);
+    }
+
+    public boolean isPressed() {
+        // Returns true if the magnet is detected, false if not
+        return !magneticLimitSensor.getState();
+    }
+}
